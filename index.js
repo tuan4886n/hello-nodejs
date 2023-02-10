a8b9ed9289633f1fadd5672c5f8c820ef476c1f4
tuan4886n <tuan4886n@gmail.com>
2023-02-10 22:51:58 +0700

    change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 app.get('/contact',function(req, res){
-    res.send('Hello World');
+    res.send('Hello World 1');
 });
 app.listen(3000, function(){
     console.log('Example app listening on port 3000!');
